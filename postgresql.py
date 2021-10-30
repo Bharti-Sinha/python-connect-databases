2f34e106a974a52acb4455f5b0be719a133810e2
Bharti Sinha <bhartisinha2322@gmail.com>
2021-10-30 09:10:45 +0530

    added queries

--- postgresql.py
+++ postgresql.py
@@ -2,12 +2,6 @@
 author: Bharti Sinha
 date: 29th October 2021
 
-# Steps for intial setup
-1.
-2.
-3.
-4.
-
 '''
 
 # import library
@@ -17,7 +11,7 @@ def create_table():
     '''
     The method connects to an already existing database
     '''
-    conn = psycopg2.connect("dbname='database1', user='postgres', password='postgres123' host='localhost' port='5432'")
+    conn = psycopg2.connect("dbname='database1', user='postgres', password='postgres' host='localhost' port='5432'")
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
     conn.commit()
@@ -33,7 +27,7 @@ def insert(item, quantity, price):
             price: price of the item
 
     '''
-    conn = psycopg2.connect("dbname='database1', user='postgres', password='postgres123' host='localhost' port='5432'")
+    conn = psycopg2.connect("dbname='database1', user='postgres', password='postgres' host='localhost' port='5432'")
     cur = conn.cursor()
     cur.execute("INSERT INTO store VALUES(%s,%s,%s)", (item, quantity, price))
     conn.commit()
@@ -45,7 +39,7 @@ def view():
     The method fetches all the rows from the database and prints it as a python list
 
     '''
-    conn = psycopg2.connect("dbname='database1', user='postgres', password='postgres123' host='localhost' port='5432'")
+    conn = psycopg2.connect("dbname='database1', user='postgres', password='postgres' host='localhost' port='5432'")
     cur = conn.cursor()
     # select all columns from the table
     cur.execute("SELECT * FROM store")
@@ -61,7 +55,7 @@ def delete(item):
     as passed in the parameter
 
     '''
-    conn = psycopg2.connect("dbname='database1', user='postgres', password='postgres123' host='localhost' port='5432'")
+    conn = psycopg2.connect("dbname='database1', user='postgres', password='postgres' host='localhost' port='5432'")
     cur = conn.cursor()
     # delete column values from the table where item value=parameter
     cur.execute("DELETE FROM store WHERE item=%s", (item,))
@@ -89,24 +83,24 @@ create_table()
 print(view())
 
 
-# # insert some values
-# insert("Coffee", 2, 3)
-# insert("Coffee", 2, 3)
-# insert("Tea", 1, 2.5)
-# insert("Cookies", 4, 5)
-# # view the table values
-# print("view after inserting values: ",view())
+# insert some values
+insert("Coffee", 2, 3)
+insert("Coffee", 2, 3)
+insert("Tea", 1, 2.5)
+insert("Cookies", 4, 5)
+# view the table values
+print("view after inserting values: ",view())
 
-# # delete an entry
-# delete("Coffee")
-# # view the table values again
-# print("view after deleting rows with 'coffee' as item values: ",view())
+# delete an entry
+delete("Coffee")
+# view the table values again
+print("view after deleting rows with 'coffee' as item values: ",view())
 
 
-# # update an entry
-# update(3, 7, "Cookies")
-# # view the table values again
-# print("view after updating 'cookies' rows: ", view())
+# update an entry
+update(3, 7, "Cookies")
+# view the table values again
+print("view after updating 'cookies' rows: ", view())
 
 
 
